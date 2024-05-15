647754f9a312a3c37c8270e942a6b7fb93e94ad9
Samuel Charlet <mail@mail>
2024-05-15 14:48:48 +0200

    ChangeCamButton

--- Math_Jam_15_05_2024/Assets/CameraMovement.cs
+++ Math_Jam_15_05_2024/Assets/CameraMovement.cs
@@ -1,18 +1,72 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static CameraMovement Instance;
+    
+    [SerializeField] private List<CamMovInfos> _camMovInfos = new List<CamMovInfos>();
+    [SerializeField] private float _timeRota = 1;
+
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        // ChangeDir((Directions)3);
     }
+
+    public void ChangeDir(Directions newDir)
+    {
+        foreach (var info in _camMovInfos)
+        {
+            if (info.Directions == newDir)
+            {
+                MoveCam(info.Rotation);
+                break;
+            }
+        }
+    }
+
+    private void MoveCam(Vector3 newRota)
+    {
+        gameObject.transform.DOKill();
+        gameObject.transform.DORotate(newRota, _timeRota);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Keypad0))
+            ChangeDir((Directions)0);
+        if(Input.GetKeyDown(KeyCode.Keypad1))
+            ChangeDir((Directions)1);
+        if(Input.GetKeyDown(KeyCode.Keypad2))
+            ChangeDir((Directions)2);
+        if(Input.GetKeyDown(KeyCode.Keypad3))
+            ChangeDir((Directions)3);
+        if(Input.GetKeyDown(KeyCode.Keypad4))
+            ChangeDir((Directions)4);
+    }
+}
+
+[Serializable]
+public class CamMovInfos
+{
+    public Directions Directions;
+    public Vector3 Rotation;
+}
+
+public enum Directions
+{
+    Left = 0,
+    Front = 1,
+    Right = 2,
+    Below = 3,
+    Top = 4
 }
---
+++ Math_Jam_15_05_2024/Assets/ChangeCamBtn.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamBtn : MonoBehaviour
+{
+    [SerializeField] private Directions _direction;
+
+    private void OnMouseDown()
+    {
+        CameraMovement.Instance.ChangeDir(_direction);
+    }
+}
