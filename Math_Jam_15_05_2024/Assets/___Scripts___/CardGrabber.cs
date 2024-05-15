8e8bfc4ba75cb6eaa7d03a9a90dd054c7a8e552f
P3D\hugo.lestriez <lestriehug@gmail.com>
2024-05-15 17:16:40 +0200

    Velocity test

--- Math_Jam_15_05_2024/Assets/___Scripts___/CardGrabber.cs
+++ Math_Jam_15_05_2024/Assets/___Scripts___/CardGrabber.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CardGrabber : MonoBehaviour
@@ -11,6 +14,7 @@ public class CardGrabber : MonoBehaviour
     CardMoveManager _manager;
     Camera _camera;
     GameObject _selectedObject;
+    float _speedSelectedObject;
 
     private RaycastHit CastRay()
     {
@@ -26,30 +30,33 @@ public class CardGrabber : MonoBehaviour
 
     private void CheckPressMouse()
     {
+        //prendre la card 
         if (Input.GetMouseButtonDown(0))
         {
             if (_selectedObject == null)
             {
                 RaycastHit hit = CastRay();
 
-                // if (hit == null) return;
-
                 if (hit.collider != null)
                 {
+                    // verif si obj est bien une card 
                     if (!hit.collider.gameObject.GetComponent<CardStacks>())
                     {
                         return;
                     }
 
+                    // assigner la card 
                     _selectedObject = hit.collider.gameObject.transform.parent.gameObject;
                     _selectedObject.GetComponent<Rigidbody>().freezeRotation = false;
                     if (_wantCursorVisibility) Cursor.visible = false;
                 }
                 else
                 {
+                    // si la card est nulle return
                     if (_selectedObject == null) return;
 
-                    //_selectedObject.GetComponent<Rigidbody>().freezeRotation = false;
+                    // bouger la card 
+
                     Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y,
                         _camera.WorldToScreenPoint(_selectedObject.transform.position).z);
                     Vector3 worldPosition = _camera.ScreenToWorldPoint(position);
@@ -60,22 +67,39 @@ public class CardGrabber : MonoBehaviour
                 }
             }
         }
+        // relacher la card 
         else if (Input.GetMouseButtonUp(0))
         {
             if (_selectedObject == null) return;
 
+            _selectedObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
             _selectedObject.transform.DORotate(new Vector3(0, 180, 0), 0);
             _selectedObject.GetComponent<Rigidbody>().freezeRotation = true;
             _selectedObject = null;
             if (_wantCursorVisibility) Cursor.visible = true;
         }
 
+        // verif si la card n'est pas nulle 
         if (_selectedObject != null)
         {
             Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y,
                 _camera.WorldToScreenPoint(_selectedObject.transform.position).z);
             Vector3 worldPosition = _camera.ScreenToWorldPoint(position);
             _selectedObject.transform.position = new Vector3(worldPosition.x, _heightToHoldCard, worldPosition.z);
+        }
+    }
+    private void VelocitySelectedObject()
+    {
+        if(_selectedObject != null)
+        {
+            _speedSelectedObject = _selectedObject.GetComponent<Rigidbody>().velocity.magnitude;
+        }
+    }
+    private void RotateCard()
+    {
+        if(_selectedObject != null)
+        {
+    
         }
     }
 
@@ -88,5 +112,7 @@ public class CardGrabber : MonoBehaviour
     private void Update()
     {
         CheckPressMouse();
+        //VelocitySelectedObject();
+        //RotateCard();
     }
 }
