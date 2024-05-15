3dc0637f61eb9a6db3628dc9438afb3e1bc4f1b8
P3D\hugo.lestriez <lestriehug@gmail.com>
2024-05-15 10:37:03 +0200

    Ajout drag and drop

--- Math_Jam_15_05_2024/Assets/___Scripts___/CardMove.cs
+++ Math_Jam_15_05_2024/Assets/___Scripts___/CardMove.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class CardMove : MonoBehaviour
 {
-    
+    Vector3 _mousePos;
+    Camera _camera;
+    Manager _manager;
+    private Vector3 GetMousePos()
+    {
+        return  _manager.Camera.WorldToScreenPoint(transform.position);
+    }
+    private void OnMouseDown()
+    {
+        _mousePos = Input.mousePosition - GetMousePos();
+    }
+    private void OnMouseDrag()
+    {
+        transform.position = _manager.Camera.ScreenToWorldPoint(Input.mousePosition - _mousePos);
+    }
+    private void Start()
+    {
+        _manager = FindObjectOfType<Manager>();
+    }
 }
