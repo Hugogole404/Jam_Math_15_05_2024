188d047fe2fc5b8abc5477284c05e0836549a226
Samuel Charlet <mail@mail>
2024-05-16 16:01:50 +0200

    Correction card setup

--- Math_Jam_15_05_2024/Assets/___Scripts___/CardMathChara.cs
+++ Math_Jam_15_05_2024/Assets/___Scripts___/CardMathChara.cs
@@ -6,8 +6,16 @@ using UnityEngine;
 public class CardMathChara : Card
 {
     [field:SerializeField] public SliderManager SliderMgr { get; set; }
+    [SerializeField] private Renderer _renderer;
+    [SerializeField] private Sprite _spriteMath;
+
     void Start()
     {
         InitAnim();
+        
+        Material material = _renderer.material;
+        material.mainTexture = _spriteMath.texture;
+        
+        // SetText("");
     }
 }
